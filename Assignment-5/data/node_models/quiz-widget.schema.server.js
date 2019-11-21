a93418504cafc2d15a66558b7c6a26d4f4164972
JayHee <55821093+JayHee@users.noreply.github.com>
2019-11-21 00:05:53 -0500

    Delete quiz-widget.schema.server.js

--- Assignment-5/data/node_models/quiz-widget.schema.server.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const questionSchema = require('./question.schema.server')
-const questionWidgetSchema = mongoose.Schema({
-    questions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'QuestionModel'
-    }]
-}, {collection: 'question-widgets'})
